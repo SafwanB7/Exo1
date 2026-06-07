0612dad436d7f30f423e2a1f96e7f1813efa2cd7
Safwan Bourbian <sb7x6z@gmail.com>
2026-06-07 23:48:56 +0200

    Exo pair/impair + somme

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 using System;
 class Program{
 	static void Main(string[] args){
-		Console.WriteLine("Deuxieme version du programme");
+		int nombre;
+		int somme= 0;
+		nombre=int.Parse(Console.ReadLine());
+		if (nombre % 2 == 0){
+			Console.WriteLine("le nombre est paire");
+		}
+		else{
+			Console.WriteLine("le nombre est impaire");
+		}
+		for(int i = 1 ; i <= nombre ; i++){
+			somme = somme + i ;
+		}
+		Console.WriteLine("la somme de tout les nombre jusqua " + nombre + "est de "+ somme);
 	}
 }
--- obj/Debug/net10.0/Exo1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Exo1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2cd5878a21aaa7bda483f3246d5e719669a3ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dead01a3f9d3c17482c746a3d68c8e245ac8db51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
